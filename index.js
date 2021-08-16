2525e7d09cea901494e02e26e6cd6f09d319e3b3
fermartinotti <fermartinotti@gmail.com>
2021-08-16 18:26:55 -0300

    Se pushea primer version del bot
    No incluye integracion con coinmarket ni archivo de conf de entorno.
    Se agregara .env file usando dotenv luego.

---
+++ index.js
@@ -0,0 +1,51 @@
+const config = require("./config.json");
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var commandBody;
+var args;
+var command;
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+const { prefix, token, currency_prefix } = config;
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if(message.content.startsWith(prefix) || message.content.startsWith(currency_prefix)){
+    commandBody = message.content.slice(prefix.length);
+    args = commandBody.split(' ');
+    command = args.shift().toLowerCase();
+  }
+
+  if(message.content.startsWith(prefix)){
+    comando(command, message);
+  }else if(message.content.startsWith(currency_prefix)){
+    precio(command, message);
+  }else{
+    return;
+  }
+
+});
+
+function comando(command, message){
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Este mensaje tuvo una latencia de ${timeTaken}ms.`);
+  }else if (command === "experto"){
+    message.channel.send(`No lo se ${message.author}, parece falso`);
+  }
+}
+
+function precio(command, message){
+  message.channel.send(`Usted pidio el precio de ${command}`);
+}
+
+
+client.login(token);
+
+
+
